6be0c3e042203106d74f2b5ab2d91f18036413e7
rev-kohei.yaeo <yaeok@revolveok.co.jp>
2023-08-10 14:14:39 +0900

    ダミーデータのサンプルを1件追加

---
+++ src/common/constants/question.dummy.ts
@@ -0,0 +1,14 @@
+/** ダミーの問題データ */
+export const DUMMY_QUESTION = [
+  {
+    quizId: 1,
+    quiz: '問題1',
+    choiceList: [
+      { id: 'A', choise: '選択肢1' },
+      { id: 'B', choise: '選択肢2' },
+      { id: 'C', choise: '選択肢3' },
+      { id: 'D', choise: '選択肢4' },
+    ],
+    answerList: ['A', 'C'],
+  },
+]
