2350f28c9882785dc7cc2bf187fd7b71ce8e0d5a
sherlockian4869 <k.yaeo@carep-tech.com>
2023-08-11 22:27:34 +0900

    ホーム画面の実装および画面遷移

--- src/app/home/[type_id]/page.tsx
+++ src/app/home/[type_id]/page.tsx
@@ -5,6 +5,16 @@
  * @screenname ExamListScreen
  * @discription 資格種類を一覧表示する画面
  */
-export default function ExamListScreen() {
-  return
+
+type Props = {
+  params: {
+    type_id: string
+  }
+}
+export default function ExamListScreen({ params }: Props) {
+  return (
+    <div>
+      <h1>{params.type_id}</h1>
+    </div>
+  )
 }
---
+++ src/app/home/layout.tsx
@@ -0,0 +1,17 @@
+import Footer from '@/components/footer.component'
+import Header from '@/components/header.component'
+import Main from '@/components/main.component'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <Header />
+      <Main>{children}</Main>
+      <Footer />
+    </>
+  )
+}
