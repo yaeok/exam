5a5788677fc5e02dc2ed82c55324f507214f0d7b
rev-kohei.yaeo <yaeok@revolveok.co.jp>
2023-08-18 16:49:00 +0900

    出題問題をランダムに出力する

--- src/lib/firebase/api/questions.ts
+++ src/lib/firebase/api/questions.ts
@@ -1,6 +1,14 @@
-import { collection, getDocs, limit, query, where } from 'firebase/firestore'
+import {
+  collection,
+  getCountFromServer,
+  getDocs,
+  limit,
+  query,
+  where,
+} from 'firebase/firestore'
 
 import { Question } from '@/common/models/question.model'
+import { generateUniqueRandomNumbers } from '@/common/utils/generate'
 import { db } from '@/lib/config'
 
 /**
@@ -12,16 +20,30 @@ export const getQuestionsByExamTypeId = async (args: {
   examTypeId: string
   limit: number
 }): Promise<Question[]> => {
+  /** 選択した資格のdocumentIdを取得する */
   const colRef = collection(db, 'questions')
   const q = query(colRef, where('pltType', 'array-contains', args.examTypeId))
   const querySnapshot = await getDocs(q)
+  /** 取得したdocumentIdをもとに問題コレクションにアクセスする */
   const questionColRef = collection(
     db,
     'questions',
     querySnapshot.docs[0].id,
     args.examTypeId
   )
-  const questionQuery = query(questionColRef, limit(args.limit))
+  const countOfQuestions: number = await getCountFromServer(
+    questionColRef
+  ).then((snapshot) => snapshot.data().count)
+  const randomNumbers: number[] = generateUniqueRandomNumbers({
+    max: countOfQuestions,
+    min: 1,
+    count: args.limit,
+  })
+  const questionQuery = query(
+    questionColRef,
+    where('questionId', 'in', randomNumbers),
+    limit(args.limit)
+  )
   const questionQuerySnapshot = await getDocs(questionQuery)
   const questions: Question[] = questionQuerySnapshot.docs.map((doc) => {
     return {
@@ -32,5 +54,8 @@ export const getQuestionsByExamTypeId = async (args: {
       explanation: doc.data().explanation,
     }
   })
+  console.log(countOfQuestions)
+  console.log(randomNumbers)
+  console.log(questions)
   return questions
 }
